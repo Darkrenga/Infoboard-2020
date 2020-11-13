9f9a005fce2e365f9b4d1d63d15089f3b88368e3
Markus-js <61780685+Markus-js@users.noreply.github.com>
2020-11-13 13:06:23 +0100

    Update vejr.js

--- Public/JS/vejr.js
+++ Public/JS/vejr.js
@@ -1,18 +1,27 @@
+// Update time 
+setInterval(updateClock, 1000);
+
 
 // PROXY => CORS Anywhere helps with accessing data from other websites that is normally forbidden by the same origin policy of web browsers. This is done by proxying requests to these sites via a server (written in Node.js, in this case).
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 
 // API's
 // 1§ vejr eu api
-let vejrEu = `${proxy}https://vejr.eu/api.php?location=Aalborg&degree=C%27))`; //henter API ned
+let vejrEuUrl = `${proxy}https://vejr.eu/api.php?location=Aalborg&degree=C%27))`; //henter API ned
 // §2 DarkSky api
-let darkSky = `${proxy}https://api.darksky.net/forecast/2f35c97a3f5b8edad9aecb3fa76058cb/57.0488,9.9217`;
+let darkSkyUrl = `${proxy}https://api.darksky.net/forecast/2f35c97a3f5b8edad9aecb3fa76058cb/57.0488,9.9217`;
+
+let getWeatherData = fetchData(vejrEuUrl, darkSkyUrl)
 console.log("fetching data");
 
 
-/// 1§ VEJR EU API
+
+
+
+function fecthData (apiUrl1, apiUrl2) {
+    /// 1§ VEJR EU API
 // GET => temperature & skyText
-fetch(vejrEu) 
+fetch(apiUrl1) 
 .then((response) => {
     return response.json(); 
 })
@@ -21,15 +30,7 @@ fetch(vejrEu)
     // Declare data 
     const {temperature, skyText} = data.CurrentData;
     
-    // Declare new Date
-    let today = new Date();
-    let hr = today.getHours();
-    let min = today.getMinutes();
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    let yyyy = today.getFullYear();
-
-    buildView(temperature, skyText, hr, min, dd, mm, yyyy)
+    buildView(temperature, skyText)
 })
 .catch(
 );
@@ -37,7 +38,7 @@ fetch(vejrEu)
 
 /// 2§ DARK SKY API
 // GET => Icon
-fetch(darkSky) 
+fetch(apiUrl2) 
 .then((response) => {
     return response.json(); 
 })
@@ -47,24 +48,51 @@ fetch(darkSky)
 })
 .catch(
 );
+}
 
 
 
 /// View ///
 
-function buildView(temperature, skyText, hr, min, dd, mm, yyyy) {
-    // Date Structure
-    today = `${dd}-${mm}-${yyyy}`;
-    // Set value
+function buildView(temperature, skyText) {
     
-    
-    document.querySelector('.time').innerHTML = today;
-    document.querySelector('.date').innerHTML = `${hr}:${min}`;
     document.querySelector('.temp').innerHTML = temperature + '&#176';
     document.querySelector('.summary').innerHTML = skyText;
     
+    // Update weather every 30min
+    setTimeout(() => {
+        getWeatherData(vejrEuUrl, darkSkyUrl);
+    }, 18000000);
+}
+
+function updateClock() {
+    // Declare new Date
+    let days = ['Søn', 'Man', 'Tir', 'Ons', 'Tors', 'Fri', 'Lør'];
+    let  months = ["Jan", "Feb", "Mar", "Apr", "Maj", "jun", "jul", "Aug", "Sep", "Okt", "Nov", "Dec"];
+    
+
+    let today = new Date();
+    let monthName=months[today.getMonth()]; // Get current month
+    let dayName = days[today.getDay()]; // Get current day of week
+    let hr = String(today.getHours()).padStart(2, '0');
+    let min = String(today.getMinutes()).padStart(2, '0');
+    let dd = String(today.getDate()).padStart(2, '0');
+    month = `${dayName} ${dd} ${monthName}`;
+
+    para = ':';
+    document.querySelector('.month').innerHTML = month;
+    document.querySelector('.hour').innerHTML = `${hr}`;
+    document.querySelector('.para').innerHTML = `${para}`;
+    document.querySelector('.minutes').innerHTML = `${min}`;
+
+
+    
+    
+
+
     
 }
+
 /// Icon
 function buildIcon(data) {
     const {icon} = data.currently;                  
@@ -108,3 +136,4 @@ function getIcon(icon) {
     
     */
 }
+
